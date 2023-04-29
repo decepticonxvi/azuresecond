5c32b8362dae4c9625f94194c13f6688b8936134
sagar jambagi <sumayajambagi1@gmail.com>
2023-04-29 20:16:47 +0530

    second

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 80;
 
-const server = app.listen(port, () =>
+const server = app.listen(port, "0.0.0.0", () =>
   console.log("Server listening on port " + port)
 );
 
